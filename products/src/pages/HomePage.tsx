c46cc47ebea9b76ba4647e4105a54231d045c4d9
Keval <keval.rana@mindinventory.com>
2025-02-21 19:47:04 +0530

    Update code

--- products/src/pages/HomePage.tsx
+++
@@ -1,6 +0,0 @@
-
-const HomePage = () => {
-    return <h1>Home Page from remote Next.js App</h1>;
-};
-
-export default HomePage;
--- products/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-
-const Home = () => {
-  return <h1>Home Page from Next.js App</h1>;
-};
-
-export default Home;
